90c6054469d442e16fac76bad71b4081fe860282
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-03-29 21:49:54 +0300

    feat: add getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -55,8 +55,14 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  const validPromises = promises.map((p) => p.catch(() => undefined));
+  return Promise.race(validPromises).then((res) => {
+    if (res === undefined) {
+      throw new Error('All promises were rejected');
+    }
+    return res;
+  });
 }
 
 /**
