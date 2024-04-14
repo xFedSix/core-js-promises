42f7783386f94ea5fb0fbe0e33651559b6861240
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-04-14 11:42:46 +0300

    feat: add getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -125,8 +125,11 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const handledPromises = promises.map((p) =>
+    p.then((value) => value).catch(() => null)
+  );
+  return Promise.all(handledPromises);
 }
 
 /**
